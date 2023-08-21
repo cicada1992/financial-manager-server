5fc8c05a976199340fbb2ad9d267795e75f6c09a
hanyoungjae <han6318@naver.com>
2023-08-21 21:19:09 +0900

    feat: DB 정보 환경변수화

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,11 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      OAUTH_GOOGLE_ID: string;
-      OAUTH_GOOGLE_SECRET: string;
-      OAUTH_GOOGLE_REDIRECT: string;
       JWT_KEY: string;
       PORT: number;
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_USER: string;
+      DB_PASSWORD: string;
     }
   }
 }
