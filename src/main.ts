ebdddcf7184fba5a005e860772999dcfdd9dcd2c
hanyoungjae <han6318@naver.com>
2023-08-22 21:55:01 +0900

    feat: CORS 좁히기 origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['https://financial-ledger.com', 'http://localhost:3000'],
     methods: 'GET, PUT, POST, DELETE',
   });
   console.log(`listen port ${process.env.PORT}`);
